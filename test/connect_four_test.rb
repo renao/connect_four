91e52cff41e29a17f0f5109d112692f61e30f2ae
René Wernicke <git@renao.de>
2013-03-20 21:09:14 +0100

    Test Commit inside TestFolder.
    New testCommit.txt added.

--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -1,7 +1,25 @@
-require 'test_helper'
+#require 'test_helper'
+require 'minitest/autorun'
+require_relative '../lib/connect_four'
 
-class ConnectFourTest < ConnectFourSpec
-  def test_hello_world
-    assert_equal "hello world", ConnectFour.hello_world
-  end
+## Minitest-Klasse zum ConnectFour Projekt 
+#
+# 2013 René Wernicke
+
+describe "ConnectFour", "Erster Testlauf mit Minitest" do 
+
+	it "Hello-World gibt den richtigen Text aus" do
+	
+	cf = ConnectFour.new
+	cf.hello_world "hello world".must_equal "hello world"
+	end
 end
+
+#class ConnectFourTest < ConnectFourSpec
+  
+  ## Hello-World-Tests
+ # def test_hello_world
+    #assert_equal "hello world", ConnectFour.hello_world	"hello world"
+	#refute_equal "", ConnectFour.hello_world "hello world"
+ # end
+#end
