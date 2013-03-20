00c1ca2dac771bdab9e3ef1f557a8767d254d4c5
René Wernicke <git@renao.de>
2013-03-20 14:57:51 +0100

    connect_four minitest spec Test
    Added a spec Test for the connect_four class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,18 @@
-module ConnectFour
-  def self.hello_world
-    "hello world"
-  end
+## ConnectFour -- Beispielprojekt --
+# 
+# Dieses Modul beschreibt das allseits bekannte "Vier gewinnt" in einer Konsolen-Anwendung für die neopoly GmbH
+#
+# 2013 René Wernicke
+
+class ConnectFour
+
+	# Erzeugt ein Vier-Gewinnt-Spiel
+	def initialize
+		# nothing to do so far...
+	end
+	
+	# Erstellt eine Hello-World-Ausgabe in der Konsole
+	def hello_world(text)
+		"#{text}"
+	end
 end
---
+++ test/connect_four_spectest.rb
@@ -0,0 +1,16 @@
+#require 'test_helper'
+require 'minitest/autorun'
+require_relative '../lib/connect_four'
+
+## Minitest-Klasse zum ConnectFour Projekt 
+#
+# 2013 René Wernicke
+
+describe "ConnectFour", "Erster Testlauf mit Minitest" do 
+
+	it "Hello-World gibt den richtigen Text aus" do
+	
+	cf = ConnectFour.new
+	cf.hello_world "hello world".must_equal "hello world"
+	end
+end
